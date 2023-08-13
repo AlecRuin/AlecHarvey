fe01630302069ffe7ed3a6569dbd22690734404c
Alec Harvey <ruinhard@gmail.com>
2023-08-13 12:07:35 -0500

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,16 +1,14 @@
-import me from "../images/me.png"
+//import me from "../images/me.png"
 const Homepage=()=>{
     return(
         <>
         <div className="large-box">
-            <img src={me} className="picture" alt="profile of me"></img>
         </div>
             
             <div className="half-height large-box large-font">
                 <h1 className="primary-text-color">HEYO!<br></br>I'm <span className="highlight-color">Alec</span></h1>
             </div>
             <div className="small-box">
-                <h3 className="primary-text-color">I WANNA LEARN IT ALL!</h3>
             </div>
         </>
     )
